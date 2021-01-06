1e7bdebe78c45a416d959d1462a9174b0877242a
nazneen1022 <nazneen1022@gmail.com>
2021-01-06 17:43:41 +0100

    add plugin details to config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,5 +13,13 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
   ],
 }
